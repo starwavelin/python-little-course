4735e1179745f752517a0bfa14b6962f4e535c54
starwavelin <benjaminxianlin@gmail.com>
2020-05-03 16:16:54 -0400

    added solution 2 to exer2 of chapter 7

--- chapter_07/exer2.py
+++ chapter_07/exer2.py
@@ -1,4 +1,4 @@
-fname = input('Please enter your file name:')
+fname = input('Please enter your file name:') # ie. mbox-short.txt
 try:
     finput = open(fname)
 except:
@@ -14,3 +14,20 @@ for line in finput:
 
 avg = spam_conf / count
 print('Average spam confidence:', avg)
+
+
+# Solution 2
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print(fname, 'cannot be found!')
+    exit()
+
+spam_conf = 0.0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    spam_conf += float(line[line.find(':') + 1 :].strip())
+    count += 1
+print("Average spam confidence:", spam_conf / count)
